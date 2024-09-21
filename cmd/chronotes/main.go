09114f0467fbada810919231d370e40b7de0e08e
yashikota <52403688+yashikota@users.noreply.github.com>
2024-09-22 04:51:43 +0900

    add allowed method

--- cmd/chronotes/main.go
+++ cmd/chronotes/main.go
@@ -26,7 +26,7 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
